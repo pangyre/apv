399bb3bd7f4c4494ae9d3dfc6f748c3431d0811b
Ashley Pond V <ashley@cpan.org>
2017-06-10 19:07:10 -0400

    What, what.

--- bin/css-validator.pl
+++ bin/css-validator.pl
@@ -2,14 +2,36 @@
 use strictures;
 use WebService::Validator::CSS::W3C;
 use Path::Tiny; # <-- Slurp file/page to check. LWP to get?
+use Scalar::Util "blessed";
+use LWP::Simple;
 
-my $css = "p { color: not-a-color }";
+my $thing = shift || die "Give a file or webpage to vaidate";
+$thing = -e $thing ? path($thing) : URI->new($thing);
+
 my $val = WebService::Validator::CSS::W3C->new;
-my $ok = $val->validate(string => $css);
+
+my $string = $thing->can("host") ? get($thing) : $thing->slurp;
+my $ok = $val->validate( profile => "css3",
+                          string => $string );
+
 
 if ( $ok and ! $val->is_valid )
 {
     print "Errors:\n";
-    printf "  * %s\n", $_->{message}
-        for $val->errors;
+    for my $error ( $val->errors )
+    {
+        $error->{message} =~ s/\A\s+|\s+\z//g;
+        next if $error->{message} eq "Parse Error";
+        printf "  * %s\n", $error->{message};
+    }
 }
+elsif ( $ok )
+{
+    print "OK!\n";
+}
+else
+{
+    print "Uncaught error...\n";
+}
+
+print $thing, $/;
